b5b72eae922acf4dbd267477e7193fff4d857d3e
Adekunle Oluwatobiloba <146554573+Destiny27052005@users.noreply.github.com>
2025-08-15 00:27:11 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,37 @@
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '80px',
+    duration: 2000,
+    reset: true
+})
+
+
+sr.reveal('.section1', {})
+sr.reveal('.buttons', { interval: 200 })
+sr.reveal('.section_title', { delay: 200 })
+sr.reveal('.section_subtitle', { delay: 400 })
+sr.reveal('.img', { delay: 200 })
+
+
+sr.reveal('.section2', {})
+sr.reveal('.section2_title', { delay: 200 })
+sr.reveal('.section2_subtitle', { delay: 400 })
+sr.reveal('.section2_box', { interval: 200 })
+
+sr.reveal('.section3', {})
+sr.reveal('.section3_title', { delay: 200 })
+sr.reveal('.section3_subtitle', { delay: 400 })
+sr.reveal('.li', { interval: 200 })
+sr.reveal('.small', { interval: 200 })
+
+
+sr.reveal('.section4', {})
+sr.reveal('.section4_title', { delay: 200 })
+sr.reveal('.section4_subtitle', { delay: 400 })
+sr.reveal('.each_contact', { interval: 200 })
+
+sr.reveal('.request', {})
+sr.reveal('.form', { interval: 200 })
+sr.reveal('label', { interval: 400 })
+
+sr.reveal('.social_icons', { interval: 200 })
